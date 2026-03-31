553a1a23f26620b00d9299365cf30b2bb31788eb
jonathanhokj <jonathanhokaijian@gmail.com>
2026-03-31 20:58:29 +0800

    add rejection_ABC_finalized_summary.py

--- rejection_ABC_finalized_summary.py
+++ rejection_ABC_finalized_summary.py
@@ -1,9 +1,9 @@
 """
-Rejection ABC Updated.
-This is the second Basic Rejection ABC algorithm.
-The code is mostly the same as the first, except
-that we add a new summary statistic for the ratio
-for infection proportion.
+Rejection ABC Finalized Summary.
+This is the third Basic Rejection ABC algorithm.
+The code is mostly the same as the second, except
+that we add two new summary statistics for the max
+degree and variance of degree.
 """
 
 import numpy as np
@@ -31,7 +31,19 @@ rewire_max = np.array(
     [np.max(split_rewiring[i]['rewire_count']) for i in range(40)])
 infected_timemax = np.array(
     [np.argmax(split_infected[i]['infected_fraction']) for i in range(40)])
-
+degree_max = np.array([
+    v.loc[v['count'] > 0, 'degree'].max()
+    for k, v in split_degree.items()
+])
+degree_var = [
+    np.average(
+        (split_degree[k]['degree']
+         - np.average(split_degree[k]['degree'],
+                      weights=split_degree[k]['count']))**2,
+        weights=split_degree[k]['count']
+    )
+    for k in sorted(split_degree)
+]
 
 # Fix the number of simulations.
 Nsim = 50000
@@ -67,15 +79,26 @@ infr_sim = np.max(infected_sim, axis=1) / \
     infected_sim[np.arange(infected_sim.shape[0]),
                  np.argmax(infected_sim, axis=1)-1]
 tmax_sim = np.argmax(infected_sim, axis=1)
+degmax_sim = np.array([np.where(hist > 0)[0].max() for hist in degree_sim])
+vardeg_sim = [
+    np.average((np.arange(len(hist)) -
+               np.average(np.arange(len(hist)), weights=hist))**2, weights=hist)
+    for hist in degree_sim
+]
+
 sd_p = np.std(p_sim)
 sd_pr = np.std(pr_sim)
 sd_r = np.std(r_sim)
 sd_tmax = np.std(tmax_sim)
+sd_dmax = np.std(degmax_sim)
+sd_vdeg = np.std(vardeg_sim)
 d = np.sqrt(
     ((p_sim - np.mean(infected_max))/sd_p)**2 +
     ((pr_sim - np.mean(infected_argmax))/sd_pr)**2 +
     ((r_sim - np.mean(rewire_max))/sd_r)**2 +
-    ((tmax_sim - np.mean(infected_timemax))/sd_tmax)**2
+    ((tmax_sim - np.mean(infected_timemax))/sd_tmax)**2 +
+    ((degmax_sim - np.mean(degree_max))/sd_dmax)**2 +
+    ((vardeg_sim - np.mean(degree_var))/sd_vdeg)**2
 )
 
 # Accept only the 1% data
